e97536c29097d0bdc09f162f0748b9a1f5cc7a3c
liyanfeng <liyanfeng@xiaomi.com>
2018-07-03 14:53:52 +0800

    适配器模式

---
+++ Adapter.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+class Target:
+    '''
+    客户所期待的接口，目标可以是
+    具体或抽象的类，也可以是接口
+    '''
+
+    def request(self):
+        print('common request')
+
+
+class Adaptee(Target):
+    '''
+    需要适配的类
+    '''
+
+    def sepcific_request(self):
+        print('specific request')
+
+
+class Adapter(Target):
+    '''
+    通过在内部包装一个Aadptee对象，把源接口
+    抓换成目标接口
+    '''
+
+    def __init__(self, ada):
+        self.adaptee = ada
+
+    def request(self):
+        self.adaptee.sepcific_request()
+
+
+if __name__ == '__main__':
+    adaptee = Adaptee()
+    adapter = Adapter(adaptee)
+    adapter.request()
